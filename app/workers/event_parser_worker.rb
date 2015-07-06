42b8e120b06f71bcbd0b99378bdb134e9287e3ad
Travis Hunter <travis.c.hunter@gmail.com>
2015-07-05 21:42:45 -0400

    Added event parser. Added Event model.

--- app/workers/event_parser_worker.rb
+++ app/workers/event_parser_worker.rb
@@ -1,16 +1,31 @@
 class EventParserWorker
   include Sidekiq::Worker
 
-  def perform(filename)
-    html = Nokogiri::HTML(File.read(filename))
+  def perform
+    source = Net::HTTP.get('www.boweryballroom.com', '/calendar')
+    html = Nokogiri::HTML(source)
 
     calendar = html.css('.tfly-calendar').first
     calendar.css('.vevent').each do |event|
       info = event.css('.one-event')
       next unless info.present?
 
-      date = event.css('.date').css('span').first
-      puts date['title'] + ": " + info.css('.url').first.text
+      date_html = event.css('.date').css('span').first
+      date = date_html['title']
+
+      url_html = info.css('.url').first
+      title = url_html.text
+      url = url_html['href']
+
+      description_html = info.css('.supports').css('a').first
+      description = description_html.text if description_html.present?
+
+      Event.create!(
+        name: title,
+        url: url,
+        description: description,
+        date: date
+      )
     end
   end
 end

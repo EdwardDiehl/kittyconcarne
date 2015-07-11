d43c2398afa1a53f4ee46c022933fef3286862ba
Travis Hunter <travis.c.hunter@gmail.com>
2015-07-11 16:53:49 -0400

    Made the parsing meta-programmy. Set up config for bowery

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,3 +1,26 @@
 class Venue < ActiveRecord::Base
+  BOWERY = 'BOWERY'
+
   has_many :events
+
+  def self.bowery
+    find_by_code(BOWERY)
+  end
+
+  def parser_config
+    send("#{code.downcase}_config")
+  end
+
+  def bowery_config
+    {
+      base_url: 'www.boweryballroom.com',
+      path: '/calendar',
+      event_list: '.tfly-calendar',
+      event_list_item: '.vevent',
+      title: proc { |e| e.css('.one-event').css('.url').first.text },
+      description: proc { |e| e.css('.one-event').css('.supports').css('a').first.text },
+      url: proc { |e| e.css('.one-event').css('.url').first['href'] },
+      date: proc { |e| e.css('.date').css('span').first['title'] }
+    }
+  end
 end
--- app/workers/event_parser_worker.rb
+++ app/workers/event_parser_worker.rb
@@ -2,26 +2,31 @@ class EventParserWorker
   include Sidekiq::Worker
 
   def perform(venue_code)
-    venue = Venue.where(code: venue_code).first
+    @venue = Venue.find_by_code(venue_code)
     return unless venue.present?
 
-    source = Net::HTTP.get('www.boweryballroom.com', '/calendar')
-    html = Nokogiri::HTML(source)
+    @config = venue.parser_config
+    parse_events!
 
-    calendar = html.css('.tfly-calendar').first
-    calendar.css('.vevent').each do |event|
-      info = event.css('.one-event')
-      next unless info.present?
+  rescue StandardError => e
+    Rails.logger.error(e)
+  end
 
-      date_html = event.css('.date').css('span').first
-      date = date_html['title']
+  private
 
-      url_html = info.css('.url').first
-      title = url_html.text
-      url = url_html['href']
+  attr_reader :venue, :config
 
-      description_html = info.css('.supports').css('a').first
-      description = description_html.text if description_html.present?
+  def parse_events!
+    event_list.each do |event|
+      title = title_for(event)
+      url = url_for(event)
+      description = description_for(event)
+      date = date_for(event)
+
+      Rails.logger.info(event) if [title, url, description, date].any?(&:blank?)
+
+      # Require title
+      next unless title.present?
 
       Event.create!(
         name: title,
@@ -31,5 +36,47 @@ class EventParserWorker
         venue_id: venue.id
       )
     end
+  end
+
+  def event_list
+    # GET it
+    source = Net::HTTP.get(config[:base_url], config[:path])
+    html = Nokogiri::HTML(source)
+
+    # Enumerate events
+    calendar_html = html.css(config[:event_list])
+    calendar_html.css(config[:event_list_item])
+  end
+
+  # Call title proc defined in Venue config
+  def title_for(event)
+    config[:title].call(event)
+  rescue NoMethodError
+    Rails.logger.info("Could not parse title for event")
+    nil
+  end
+
+  # Call url proc defined in Venue config
+  def url_for(event)
+    config[:url].call(event)
+  rescue NoMethodError
+    Rails.logger.info("Could not parse url for event")
+    nil
+  end
+
+  # Call description proc defined in Venue config
+  def description_for(event)
+    config[:description].call(event)
+  rescue NoMethodError
+    Rails.logger.info("Could not parse description for event")
+    nil
+  end
+
+  # Call date proc defined in Venue config
+  def date_for(event)
+    config[:date].call(event)
+  rescue NoMethodError
+    Rails.logger.info("Could not parse date for event")
+    nil
   end
 end
